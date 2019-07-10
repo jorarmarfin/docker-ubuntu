4561177afcc78f14a1f6449df84ec7d96d72dffb
Luis mayta <luis.mayta@gmail.com>
2019-07-10 13:00:54 -0500

    cambio build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build -t jorarmarfin/ubuntu:tmp . 
 docker build -t jorarmarfin/ubuntu:16.04-supervisor . 
 
-docker run --name srv-ubuntu -p 9005:9001 -dit jorarmarfin/ubuntu:tmp
+docker run --name srv-ubuntu -p 9005:80 -dit jorarmarfin/ubuntu:tmp
